4241e43f28388fdb7151dd91b5771ca7ae0d8a6b
ChristianZaccaria <christian.zaccaria.cz@gmail.com>
2022-04-20 13:45:53 +0100

    Skeleton Menu for Bucket List App. ScannerInput utility implemented.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,87 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import utils.ScannerInput
+import java.lang.System.exit
 
-    // Try adding program arguments at Run/Debug configuration
-    println("Program arguments: ${args.joinToString()}")
+fun main(args: Array<String>) {
+
+    runMenu()
+
+}
+
+
+fun mainMenu() : Int {
+    return ScannerInput.readNextInt("""
+         > ------------------------------------------
+         > |            BUCKET LIST APP             |
+         > ------------------------------------------
+         > | BUCKET LIST MENU                       |
+         > |   1) Add an Experience                 |
+         > |   2) List Experiences                  |
+         > |   3) Update an Experience              |
+         > |   4) Delete an Experience              |
+         > |   5) Cross Item Off your Bucket List   |
+         > |   6) Search for an added Experience    |
+         > |  20) Save Experiences                  |
+         > |  21) Load Experiences                  |
+         > ------------------------------------------
+         > |   0) Exit                              |
+         > ------------------------------------------
+         > ==>>""".trimMargin(">"))
+}
+
+
+fun runMenu(){
+    do {
+        val option = mainMenu()
+        when (option) {
+            1 -> addExperience()
+            2 -> listExperiences()
+            3 -> updateExperience()
+            4 -> deleteExperience()
+            5 -> crossOffExperience()
+            6 -> searchExperiences()
+            20 -> save()
+            21 -> load()
+            0 -> exitApp()
+            else -> println("Invalid option entered: ${option}")
+        }
+    } while (true)
+}
+
+
+fun addExperience(){
+    println("You chose Add Experience")
+}
+
+fun listExperiences(){
+    println("You chose List Experiences")
+}
+
+fun updateExperience(){
+    println("You chose Update Experience")
+}
+
+fun deleteExperience(){
+    println("You chose Delete Experience")
+}
+
+fun crossOffExperience(){
+    println("You chose Cross Off Experience")
+}
+
+fun searchExperiences(){
+    println("You chose Search for Added Experiences")
+}
+
+
+fun save() {
+    println("Saved to File")
+}
+
+fun load() {
+    println("Loaded from File")
+}
+
+fun exitApp(){
+    println("Exiting...bye")
+    exit(0)
 }
