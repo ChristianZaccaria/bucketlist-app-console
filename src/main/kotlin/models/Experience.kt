71928e2479042c8e7fc6bb56bc80dd8f32744761
ChristianZaccaria <christian.zaccaria.cz@gmail.com>
2022-04-20 15:58:48 +0100

    Added new model class called "Experience"

---
+++ src/main/kotlin/models/Experience.kt
@@ -0,0 +1,7 @@
+package models
+
+    data class Experience (var experienceTitle: String, var experienceDescription: String,
+                           var experienceCategory: String, var dateToAchieve: String,
+                           var experiencePriority: Int, var isExperienceAchieved: Boolean) {
+
+}
