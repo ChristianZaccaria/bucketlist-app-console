331a4e551b7c828869839dac0ad32ba91f3c8f13
ChristianZaccaria <christian.zaccaria.cz@gmail.com>
2022-04-21 21:39:08 +0100

    Functionality to Save to and Load experiences from external file (JSON)

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,7 @@
 import controllers.ExperienceAPI
 import models.Experience
 import mu.KotlinLogging
+import persistence.JSONSerializer
 import persistence.XMLSerializer
 import utils.ScannerInput
 import utils.ScannerInput.readNextInt
@@ -9,7 +10,8 @@ import java.io.File
 import java.lang.System.exit
 
 private val logger = KotlinLogging.logger {}
-private val experienceAPI = ExperienceAPI(XMLSerializer(File("experiences.xml")))
+//private val experienceAPI = ExperienceAPI(XMLSerializer(File("experiences.xml")))
+private val experienceAPI = ExperienceAPI(JSONSerializer(File("experiences.json")))
 
 
 fun main(args: Array<String>) {
